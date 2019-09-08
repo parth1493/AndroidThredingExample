ca8088fedda85ec81fc5bcdf0b29e5a9dfbcbaa2
Parth <parth1493@gmail.com>
2019-09-07 23:50:08 -0400

    Project Setup

---
+++ app/src/main/java/com/parth/androidthredingexample/persistence/WordDao.java
@@ -0,0 +1,35 @@
+package com.parth.androidthredingexample.persistence;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import com.parth.androidthredingexample.models.Word;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+    @Query("SELECT * FROM Word WHERE title LIKE :title || '%'")
+    List<Word> getWords(String title);
+
+    @Query("SELECT * FROM Word")
+    List<Word> getAllWords();
+
+    @Insert
+    long[] insertWords(Word... words);
+
+    @Delete
+    int delete(Word note);
+
+    @Query("UPDATE Word SET title = :title, content = :content, timestamp = :timestamp WHERE uid = :uid")
+    int updateWord(String title, String content, String timestamp, int uid);
+
+    @Query("SELECT * FROM Word LIMIT :row, :numRows ")
+    public List<Word> getSomeWords(int row, int numRows);
+
+    @Query("SELECT COUNT(*) FROM Word")
+    public Integer getNumRows();
+}
